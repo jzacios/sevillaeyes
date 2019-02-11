18e76186d5f94467e95dd248e0f8c04446717613
Swaggin <jarzab76pl@o2.pl>
2019-02-11 11:13:57 +0100

    11:13

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-xD
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+
+        <title> SevillaEyes </title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="stylesheet" href="main/var.css">
+        <link rel="stylesheet" href="main/index.css">
+
+    </head>
+    <body>
+
+        <section class="slide-1">
+            <h1 class="title">Sevilla<span class="title-light">Eyes</span></h1>
+            <p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime sit facilis quo?</p>
+        </section>
+
+    </body>
+</html>
--- xD.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jakub
- * Date: 11.02.2019
- * Time: 11:08
- */
