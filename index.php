20b8bdfc3d1bc726f5c8367b53ee17b39fc13ace
Swaggin <jarzab76pl@o2.pl>
2019-02-11 11:33:24 +0100

    11:33

--- index.php
+++ index.php
@@ -10,10 +10,16 @@
 
         <link rel="stylesheet" href="main/var.css">
         <link rel="stylesheet" href="main/index.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" 
+            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     </head>
     <body>
-
+        <nav class="navbar">
+            <a href="#" class="option"><i class="far fa-user"></i></a>
+            <a href="#" class="option"><i class="far fa-user"></i></a>
+            <a href="#" class="option"><i class="far fa-user"></i></a>
+        </nav>
         <section class="slide-1">
             <h1 class="title">Sevilla<span class="title-light">Eyes</span></h1>
             <p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime sit facilis quo?</p>
