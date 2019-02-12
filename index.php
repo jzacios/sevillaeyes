1ff96849317150afcd9bedf2fd90984dd86b0474
Swaggin <jarzab76pl@o2.pl>
2019-02-12 10:03:07 +0100

    10:03

--- index.php
+++ index.php
@@ -13,17 +13,34 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" 
             integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
     </head>
     <body>
         <nav class="navbar">
-            <a href="#" class="option"><i class="fas fa-user"></i></a>
-            <a href="#" class="option"><i class="fas fa-user"></i></a>
-            <a href="#" class="option"><i class="fas fa-user"></i></a>
+            <div class="options" id="options">
+                <a href="#" class="option"><i class="fas fa-user"></i></a>
+                <a href="#" class="option"><i class="fas fa-user"></i></a>
+                <a href="#" class="option"><i class="fas fa-user"></i></a>
+            </div>
+            <div class="scroll-down" id="scroll-down"><i class="fas fa-angle-down"></i></div>
+            <div class="scroll-up" id="scroll-up"><i class="fas fa-angle-up"></i></div>
         </nav>
         <section class="slide-1">
             <h1 class="title">Sevilla<span class="title-light">Eyes</span></h1>
             <p class="desc">Head nudges hopped up on catnip. Attack dog, run away and pretend to be victim lick sellotape with tail in the air stare out the window thinking longingly about tuna brine so who's the baby. </p>
         </section>
+        <section class="slide-2">
 
+        </section>
+        <script>
+            $(document).ready(function() {
+                $("#scroll-down").click(function() {
+                    $("#options").css("display", "hidden");
+                    $("#scroll-down").css("display", "hidden");
+
+                });
+            });
+        </script>
     </body>
 </html>
